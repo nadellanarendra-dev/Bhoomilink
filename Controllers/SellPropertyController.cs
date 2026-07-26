e3e137585a68347c021ef2fd768354b08fa5f0cb
vishnutrillion <63408933+vishnutrillion@users.noreply.github.com>
2026-07-26 20:01:18 +0530

    new changes

---
+++ Controllers/SellPropertyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bhoomilink.Controllers
+{
+    public class SellPropertyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
